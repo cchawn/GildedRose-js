40489547f32864b25a314a53082e55d595652fc0
Christina Chan <cchan@wealthsimple.com>
2017-12-29 16:53:28 -0500

    Stub test suite

--- spec/gilded_rose.test.js
+++ spec/gilded_rose.test.js
@@ -1,9 +1,57 @@
 const { Item, Shop } = require('../src/gilded_rose');
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+describe('Gilded Rose', () => {
+  function createShop({ name, sellIn = 0, quality = 0 }) {
+    return new Shop([ new Item(name, sellIn, quality) ]);
+  }
+
+  it('reduces SellIn date by 1 each day');
+  it('reduces Quality by 1 each day');
+
+  describe('when SellIn date has passed', () => {
+    it('reduces Quality by 2 each day');
+  });
+
+  describe('when Quality is 0', () => {
+    it('does not reduce quality below 0');
+  });
+
+  describe('Aged Brie', () => {
+    it('increases Quality by 1 each day');
+  });
+
+  describe('when Quality is 50', () => {
+    it('does not increase quality above 50');
+  });
+
+  describe('when Quality is >50', () => {
+    it('cannot create the Item');
+  });
+
+  describe('Sulfuras', () => {
+    it('does not reduce Quality');
+    it('does not reduce SellIn date');
+  });
+
+  describe('Backstage passes', () => {
+    describe('when SellIn date is more than 10 days away', () => {
+      it('increases Quality by 1 each day');
+    });
+
+    describe('when SellIn date is 10 days away (or less)', () => {
+      it('increases Quality by 2 each day');
+    });
+
+    describe('when SellIn date is 5 days away (or less)', () => {
+      it('increases Quality by 3 each day');
+    });
+
+    describe('when SellIn date has passed', () => {
+      it('Quality drops to 0');
+    });
+  });
+
+  describe('Conjured items', () => {
+    it('reduces in Quality by 2 each day');
   });
 });
