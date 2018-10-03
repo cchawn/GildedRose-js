361b1af8174d0eeb84ce1ba1b549a4204f7d65f8
Christina Chan <6462356+cchawn@users.noreply.github.com>
2018-10-03 18:30:22 -0400

    Reverse backstage pass sell-in logic

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -38,16 +38,17 @@ class Shop {
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+      /** Sell In Date has passed **/
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+            this.items[i].quality = 0;
+          } else {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
