d5b9f062db80324a14e3a94e27d2e6472ac3dc20
Christina Chan <6462356+cchawn@users.noreply.github.com>
2018-10-04 10:20:25 -0400

    Set this.items[i] to a constant to avoid repetition

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -16,57 +16,62 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name !== BRIE && this.items[i].name !== PASSES) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name !== SULFURAS) {
-            this.items[i].quality = this.items[i].quality - 1;
+      const item = this.items[i];
+
+      if (item.name !== BRIE && item.name !== PASSES) {
+        if (item.quality > 0) {
+          if (item.name !== SULFURAS) {
+            item.quality = item.quality - 1;
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name === PASSES) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.name === PASSES) {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       }
-      if (this.items[i].name !== SULFURAS) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+
+      /* Decrease Sell In Date */
+      if (item.name !== SULFURAS) {
+        item.sellIn = item.sellIn - 1;
       }
+
       /** Sell In Date has passed **/
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name === BRIE) {
+      if (item.sellIn < 0) {
+        if (item.name === BRIE) {
           /* Aged Brie
            * Once the sell in date has passed,
            * increase in quality up to a maximum of 50.
            */
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
-        } else if (this.items[i].name === PASSES) {
+        } else if (item.name === PASSES) {
           /* Backstage Passes
            * Once the sell in date has passed,
            * decrease the quality to 0.
            */
-          this.items[i].quality = 0;
+          item.quality = 0;
         } else {
           /* Everything else
            * Once the sell in date has passed,
            * decrease the quality twice as fast to a minimum of 0.
            * We already decreased the quality once, do it again here.
            */
-          if (this.items[i].quality > 0) {
-            if (this.items[i].name !== SULFURAS) {
-              this.items[i].quality = this.items[i].quality - 1;
+          if (item.quality > 0) {
+            if (item.name !== SULFURAS) {
+              item.quality = item.quality - 1;
             }
           }
         }
