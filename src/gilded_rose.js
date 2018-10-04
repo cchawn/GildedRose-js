2f123b3aff4b56ce333011253ad74c9236886f86
Christina Chan <6462356+cchawn@users.noreply.github.com>
2018-10-04 10:02:33 -0400

    Improve sell in date section

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -45,17 +45,28 @@ class Shop {
       /** Sell In Date has passed **/
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name === BRIE) {
+          /* Aged Brie
+           * Once the sell in date has passed,
+           * increase in quality up to a maximum of 50.
+           */
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
+        } else if (this.items[i].name === PASSES) {
+          /* Backstage Passes
+           * Once the sell in date has passed,
+           * decrease the quality to 0.
+           */
+          this.items[i].quality = 0;
         } else {
-          if (this.items[i].name === PASSES) {
-            this.items[i].quality = 0;
-          } else {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name !== SULFURAS) {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+          /* Everything else
+           * Once the sell in date has passed,
+           * decrease the quality twice as fast to a minimum of 0.
+           * We already decreased the quality once, do it again here.
+           */
+          if (this.items[i].quality > 0) {
+            if (this.items[i].name !== SULFURAS) {
+              this.items[i].quality = this.items[i].quality - 1;
             }
           }
         }
